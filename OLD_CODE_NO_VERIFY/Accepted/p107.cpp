0f3419288f5bb9858d66f912e9148f1e7d9986a3
wbkuo <et282523@hotmail.com>
2014-10-07 22:42:34 +0800

    107 tmp

--- OLD_CODE_NO_VERIFY/Accepted/p107.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p107.cpp
@@ -1,45 +1,52 @@
-/* @JUDGE_ID:  12991WW  100  C  "Easy algorithm" */
-#include <iostream.h>
-#include <math.h>
+#include <iostream>
+#include <cmath>
+using std::cin;
+using std::cout;
 
-main(){
+int main()
+{
     long q,s;
     long d,tall,t;
     long i,j,n;
     long total,totalsum;
 
+	while (cin >>q>>s)
+	{
+		if (q==0 && s==0 ) break;
+		if (q-1 == s)
+		{
+			cout << "1 " << q+s << "\n";
+			continue;
+		}
 
-    while (cin >>q>>s){
-	if (q==0 && s==0 ) break;
-	if ( q-1 == s ){
-	    cout <<"1 "<<q+s<<"\n";
-	    continue;
+		for (i=2;i<=sqrt(q);i++)
+		{
+			tall=q;
+			t=0;
+			while (1)
+			{
+				t++;
+				d=tall%i;
+				if (d!=0) break;
+				tall=tall/i;
+				if (tall==1 && pow(i-1,t)==s )
+				{
+					//ans!
+					n=i-1;
+					tall=q;
+					total=totalsum=0;
+					for (j=1;j<=t;j++)
+					{
+						tall= q /(long)( pow(n+1,j-1));
+						total=total+ (long)pow(n,j-1);
+						totalsum=totalsum+tall* (long)pow(n,j-1);
+					}
+					totalsum=totalsum+s;
+					break;
+				}
+			}
+		}
+		cout <<total<<" "<<totalsum<<"\n";
 	}
-
-	for (i=2;i<=sqrt(q);i++){
-	   tall=q;
-	   t=0;
-	   while (1){
-	      t++;
-	      d=tall%i;
-	      if (d!=0) break;
-	      tall=tall/i;
-	      if (tall==1 && pow(i-1,t)==s ){
-		 //ans!
-		 n=i-1;
-		 tall=q;
-		 total=totalsum=0;
-		 for (j=1;j<=t;j++){
-		    tall=q/( pow(n+1,j-1));
-		    total=total+pow(n,j-1);
-		    totalsum=totalsum+tall*pow(n,j-1);
-		 }
-		 totalsum=totalsum+s;
-		 break;
-	      }
-	   }
-	}
-	cout <<total<<" "<<totalsum<<"\n";
-
-    }
+    return 0;
 }
