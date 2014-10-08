b4b07344b77013d9f6afe767bd1ff7ec33d46380
wbkuo <et282523@hotmail.com>
2014-10-09 01:00:25 +0800

    暫存

--- OLD_CODE_NO_VERIFY/Accepted/p621.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p621.cpp
@@ -1,26 +1,35 @@
-/* @JUDGE_ID:  12991WW  621  C++  */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 
-#include <stdio.h>
-#include <iostream.h>
-#include <string.h>
+using namespace std;
 
-main(){
+main()
+{
     long n;
     int len;
     char s[1000];
     cin >>n;
 
-    while (cin >>n !=NULL){
-	  sprintf(s,"%ld",n);
-	  len=strlen(s);
-	  if (strcmp("1",s) ==0 || strcmp("4",s) ==0 || strcmp("78",s)==0 ){
-		cout <<"+\n";
-	  }else if (s[len-2]=='3' && s[len-1]=='5' ){
-		cout <<"-\n";
-	  }else if (s[0]=='9' && s[len-1]=='4' ){
-		cout <<"*\n";
-	  }else if (s[0]=='1' && s[1]=='9' && s[2]=='0'){
-		cout <<"?\n";
-	  }
+    while (cin >> n != NULL)
+    {
+        sprintf(s, "%ld", n);
+        len = strlen(s);
+        if (strcmp("1", s) == 0 || strcmp("4", s) == 0 || strcmp("78", s) == 0)
+        {
+            cout << "+\n";
+        }
+        else if (s[len - 2] == '3' && s[len - 1] == '5')
+        {
+            cout << "-\n";
+        }
+        else if (s[0] == '9' && s[len - 1] == '4')
+        {
+            cout << "*\n";
+        }
+        else if (s[0] == '1' && s[1] == '9' && s[2] == '0')
+        {
+            cout << "?\n";
+        }
     }
 }
