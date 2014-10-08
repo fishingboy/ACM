66d631ff0e9d181ac12dc31bd9a1a378a7e0c91b
wbkuo <et282523@hotmail.com>
2014-10-08 22:49:31 +0800

    [Verify] 412 - Pi

---
+++ Accept/412.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int gcd(int m, int n)
+{
+    int k;
+    do
+    {
+        k = m % n;
+        m = n;
+        n = k;
+    }
+    while (k != 0);
+    return m;
+}
+
+main()
+{
+    int i, j, n;
+    double a[100];
+    double nn, pri;
+    double ans;
+
+    while (cin >> n, n != 0)
+    {
+        nn = pri = 0;
+        for (i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        for (i = 1; i <= n - 1; i++)
+        {
+            for (j = i + 1; j <= n; j++)
+            {
+                nn++;
+                if (gcd(a[i], a[j]) == 1) pri++;
+            }
+        }
+        if (pri == 0)
+        {
+            cout << "No estimate for this data set.\n";
+        }
+        else
+        {
+            ans = sqrt(6.0 * nn / pri);
+            printf("%.6lf\n", ans);
+        }
+    }
+}
