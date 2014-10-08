89ba273681e1d888ceaf4f0c7588d3ee681e4234
wbkuo-nb <et282523@hotmail.com>
2014-10-08 17:47:33 +0800

    [Verify] 369 -  Combinations

---
+++ Accept/369.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+main()
+{
+    unsigned long a[101][101];
+    unsigned long ans;
+    int i, j;
+    int n, r;
+
+    a[0][0] = 1;
+    for (i = 1; i <= 100; i ++)
+    {
+        a[i][0] = 1;
+        a[i][i] = 1;
+
+        for (j = 1; j <= i - 1; j ++)
+        {
+            a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+        }
+    }
+    
+    while (cin >> n >> r, n != 0)
+    {
+        ans = a[n][r];
+        cout << n << " things taken " << r;
+        cout << " at a time is " << ans << " exactly.\n";
+    }
+}
