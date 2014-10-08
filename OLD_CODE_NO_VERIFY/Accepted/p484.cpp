cc9a6eaf5679d82493d6231297affddd0d8031b8
wbkuo <et282523@hotmail.com>
2014-10-08 23:35:01 +0800

    暫存

--- OLD_CODE_NO_VERIFY/Accepted/p484.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p484.cpp
@@ -1,27 +1,34 @@
-/* @JUDGE_ID:  12991WW  484  C++  "Easy algorithm" */
+#include <iostream>
+#include <cstring>
 
-#include <iostream.h>
-#include <string.h>
+using namespace std;
 
-main(){
+main()
+{
     int a[1000];
     int asum[1000];
-    long t=0;
-    int n,i,j;
+    long t = 0;
+    int n, i, j;
 
-    while (cin >>n !=NULL){
-	 for (i=1;i<=t;i++){
-	     if (a[i]==n) break;
-	 }
-	 if (i>t){
-	     t++;
-	     a[t]=n;
-	     asum[t]=1;
-	 }else{
-	     asum[i]++;
-	 }
+    while (cin >> n != NULL)
+    {
+        for (i = 1; i <= t; i++)
+        {
+            if (a[i] == n) break;
+        }
+        if (i > t)
+        {
+            t++;
+            a[t] = n;
+            asum[t] = 1;
+        }
+        else
+        {
+            asum[i]++;
+        }
     }
-    for (i=1;i<=t;i++){
-	cout <<a[i]<<" "<<asum[i]<<"\n";
+    for (i = 1; i <= t; i++)
+    {
+        cout << a[i] << " " << asum[i] << "\n";
     }
 }
--- OLD_CODE_NO_VERIFY/Accepted/p490.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p490.cpp
@@ -1,34 +1,38 @@
-/* @JUDGE_ID:  12991WW  490  C++  "Easy algorithm" */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 
-#include <stdio.h>
-#include <iostream.h>
-#include <string.h>
+using namespace std;
 
-main(){
+main()
+{
     char ss[1000];
     char s[11000];
     int st[100];
-    int i,j,l;
-    int t=0,max=0;
-    while (gets(ss)){
-	t++;
-	l=strlen(ss);
-	cout <<l<<"\n";
-	for (i=0;i<=strlen(ss)-1;i++){
-	    s[t*100+i+1]=ss[i];
-	}
-	st[t]=strlen(ss);
-	if (strlen(ss)>max) max=strlen(ss);
-	if (l==0) st[t]=0;
-	if (t==3) break;
+    int i, j, l;
+    int t = 0, max = 0;
+    while (gets(ss))
+    {
+        t++;
+        l = strlen(ss);
+        for (i = 0; i <= strlen(ss) - 1; i++)
+        {
+            s[t * 100 + i + 1] = ss[i];
+        }
+        st[t] = strlen(ss);
+        if (strlen(ss) > max) max = strlen(ss);
+        if (l == 0) st[t] = 0;
+        if (t == 3) break;
     }
-    for (i=0;i<=max-1;i++){
-	for (j=t;j>=1;j--){
-	   if (i >= st[j])
-	      cout <<" ";
-	   else
-	      cout <<s[j*100+i+1];
-	}
-	cout <<"\n";
+    for (i = 0; i <= max - 1; i++)
+    {
+        for (j = t; j >= 1; j--)
+        {
+            if (i >= st[j])
+                cout << " ";
+            else
+                cout << s[j * 100 + i + 1];
+        }
+        cout << "\n";
     }
 }
