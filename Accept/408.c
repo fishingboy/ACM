64bb297e3647fcc4beb90ee969292927061dc0c5
wbkuo <et282523@hotmail.com>
2014-10-08 22:21:15 +0800

    [Verify] 408 - Uniform Generator

---
+++ Accept/408.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+long gcd(long m, long n)
+{
+    long k;
+    do
+    {
+        k = m % n;
+        m = n;
+        n = k;
+    }
+    while (k != 0);
+    return m;
+}
+
+int main()
+{
+    long step, mod;
+    while (scanf("%ld %ld", &step, &mod) == 2)
+    {
+        printf("%10ld%10ld", step, mod);
+        if (gcd(step, mod) == 1)
+            printf("    Good Choice\n\n");
+        else
+            printf("    Bad Choice\n\n");
+    }
+    return 0;
+}
