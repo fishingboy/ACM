5e3e2fd7bb1d0024bb4ed8b1ee118ff4fa6de3b5
wbkuo-nb <et282523@hotmail.com>
2014-10-08 18:08:36 +0800

    暫存

--- OLD_CODE_NO_VERIFY/Accepted/p389.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p389.cpp
@@ -1,71 +1,85 @@
-/* @JUDGE_ID:  12991WW  389  C++  "Easy algorithm" */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cctype>
 
-#include <stdio.h>
-#include <iostream.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>
+using namespace std;
 
-unsigned long bval(char a[10],int b){
-     int i,l,t=0,m=0;
-     unsigned long bb=0,sum=0;
-     l=strlen(a);
-     for (i=l-1;i>=0;i--){
-	  if (isdigit(a[i]) !=0){
-	      m=a[i]-'0';
-	  }else{
-	      m=a[i]-'A'+10;
-	  }
-	  bb=pow(b,t);
-	  t++;
-	  sum=sum+m*bb;
-     }
-      return sum;
-}
-
-void p(unsigned long val,int base){
-    char s[10]="       ";
-    int t=0;
-    long b;
-    t=0;
-
-    if (base==10){
-       printf("%7ld\n",val);
-    }else{
-       while (1==1){
-	  t++;
-	  b=val%base;
-	  val=(val-b)/base;
-
-	  if (b>=0 &&  b<=9){
-	      s[6-t+1]='0'+b;
-	  }else{
-	      s[6-t+1]='A'+b-10;
-	  }
-
-	  if (val==0) break;
-	  if (t>7) break;
-       }
-
-       if (t>7){
-	  cout <<"  ERROR\n";
-       }else{
-	  cout <<s<<"\n";
-       }
-
+unsigned long bval(char a[10], int b)
+{
+    int i, l, t = 0, m = 0;
+    unsigned long bb = 0, sum = 0;
+    l = strlen(a);
+    for (i = l - 1; i >= 0; i --)
+    {
+        if (isdigit(a[i]) != 0)
+        {
+            m = a[i] - '0';
+        }
+        else
+        {
+            m = a[i] - 'A' + 10;
+        }
+        bb = pow(b, t);
+        t ++;
+        sum = sum + m*bb;
     }
-
-
+    return sum;
 }
 
-main(){
+void p(unsigned long val, int base)
+{
+    char s[10] = "       ";
+    int t = 0;
+    long b;
+    t = 0;
+
+    if (base == 10)
+    {
+        printf("%7ld\n", val);
+    }
+    else
+    {
+        while (1 == 1)
+        {
+            t ++;
+            b = val % base;
+            val = (val - b) / base;
+
+            if (b >= 0 && b <= 9)
+            {
+                s[6 - t + 1] = '0' + b;
+            }
+            else
+            {
+                s[6 - t + 1] = 'A' + b - 10;
+            }
+
+            if (val == 0) break;
+            if (t > 7) break;
+        }
+
+        if (t > 7)
+        {
+            cout << "  ERROR\n";
+        }
+        else
+        {
+            cout << s << "\n";
+        }
+    }
+}
+
+main()
+{
     char s[10];
-    int b1,b2;
+    int b1, b2;
     unsigned long v;
 
-    while (cin>>s>>b1>>b2 !=NULL){
-	  v=bval(s,b1);
-	  p(v,b2);
-
+    while (cin >> s >> b1 >> b2)
+    {
+        v = bval(s, b1);
+        p(v, b2);
     }
 }
