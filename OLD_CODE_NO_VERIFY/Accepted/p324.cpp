af5d45f4656448ad5aba6c2e2cef476008966adf
wbkuo-nb <et282523@hotmail.com>
2014-10-08 17:29:20 +0800

    暫存

--- OLD_CODE_NO_VERIFY/Accepted/p324.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p324.cpp
@@ -1,111 +1,126 @@
-/* @JUDGE_ID:  12991WW  324  C++  "Easy algorithm" */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
-#include <stdio.h>
-#include <iostream.h>
-#include <string.h>
-#include <stdlib.h>
+using namespace std;
 
-int sum[9] ;
+int sum[9];
 
-void pp(char x[] ,int l){
-    int i,len,t;
-    len=strlen(x);
-    t=l-len;
+void pp(char x[], int l)
+{
+    int i, len, t;
+    len = strlen(x);
+    t = l - len;
 
-    for (i=l-1;i>=t;i--){
-	 x[i]=x[i-t];
+    for (i = l - 1; i >= t; i --)
+    {
+        x[i] = x[i - t];
     }
-    for (i=0;i<=t-1;i++){
-	  x[i]=48;
+    for (i = 0; i <= t - 1; i ++)
+    {
+        x[i] = 48;
     }
-    x[l]=0;
+    x[l] = 0;
 }
 
-int sval(char m){
-    return m-'0';
+int sval(char m)
+{
+    return m - '0';
 }
 
-
-
-void lmul(char  a[],int b,char c[],int l ){
-     int i;
-     unsigned long d,cy=0;
-     for (i=l-1;i>=0;i--){
-	 d=sval( a[i] );
-	 c[i]=( d*b+cy ) %10 +'0';
-	 cy= (d*b+cy)/10 ;
-     }
-     c[l]=0;
+void lmul(char a[], int b, char c[], int l)
+{
+    int i;
+    unsigned long d, cy = 0;
+    for (i = l - 1; i >= 0; i --)
+    {
+        d = sval(a[i]);
+        c[i] = (d * b + cy) % 10 + '0';
+        cy = (d * b + cy) / 10;
+    }
+    c[l] = 0;
 }
 
-void digadd( char s[]){
-     int i,be=0;
-     char m;
-     //clear !
-     for (i=0;i<=9;i++){
-	 sum[i]=0;
-     }
+void digadd(char s[])
+{
+    int i, be = 0;
+    char m;
+    
+    /* clear ! */
+    for (i = 0; i <= 9; i ++)
+    {
+        sum[i] = 0;
+    }
 
 
-     for (i=0;i<=strlen(s)-1;i++){
-	 m=s[i];
-	 if (be==0 && m!='0') be=1;
-	 if (be==1){
-	     if ( m=='0') sum[0]++;
-	     if ( m=='1') sum[1]++;
-	     if ( m=='2') sum[2]++;
-	     if ( m=='3') sum[3]++;
-	     if ( m=='4') sum[4]++;
-	     if ( m=='5') sum[5]++;
-	     if ( m=='6') sum[6]++;
-	     if ( m=='7') sum[7]++;
-	     if ( m=='8') sum[8]++;
-	     if ( m=='9') sum[9]++;
-	 }
-     }
+    for (i = 0; i <= strlen(s) - 1; i ++)
+    {
+        m = s[i];
+        if (be == 0 && m != '0') be = 1;
+        if (be == 1)
+        {
+            if (m == '0') sum[0] ++;
+            if (m == '1') sum[1] ++;
+            if (m == '2') sum[2] ++;
+            if (m == '3') sum[3] ++;
+            if (m == '4') sum[4] ++;
+            if (m == '5') sum[5] ++;
+            if (m == '6') sum[6] ++;
+            if (m == '7') sum[7] ++;
+            if (m == '8') sum[8] ++;
+            if (m == '9') sum[9] ++;
+        }
+    }
 }
 
-void chs(char s1[],char s2[] ){
-     int i;
-     for (i=0;i<=strlen(s2);i++){
-	 s1[i]=s2[i];
-     }
+void chs(char s1[], char s2[])
+{
+    int i;
+    for (i = 0; i <= strlen(s2); i ++)
+    {
+        s1[i] = s2[i];
+    }
 }
 
-int ilen(int i){
-    if (i<10) return 1;
-    if (i<100) return 2;
-    if (i<1000) return 3;
-    if (i<10000) return 4;
+int ilen(int i)
+{
+    if (i < 10) return 1;
+    if (i < 100) return 2;
+    if (i < 1000) return 3;
+    if (i < 10000) return 4;
 
 }
 
-main(){
-    char a[2000],ans[2000];
+main()
+{
+    char a[2000], ans[2000];
 
     int n;
     int f;
     int i;
     int maxl;
 
+    while (scanf("%d\n", &n))
+    {
+        if (n == 0) break;
+        a[0] = '1';
+        a[1] = 0;
+        for (i = 2; i <= n; i ++)
+        {
+            maxl = strlen(a) + ilen(i);
+            pp(a, maxl);
+            lmul(a, i, ans, maxl);
+            chs(a, ans);
+        }
+        digadd(a);
 
-    while (scanf ("%d\n",&n) ){
-	 if (n ==0) break;
-	 a[0]='1';a[1]=0;
-	 for (i=2;i<=n;i++){
-	     maxl=strlen(a)+ilen(i);
-	     pp(a,maxl);
-	     lmul(a,i,ans,maxl);
-	     chs(a,ans);
-	 }
-	 digadd(a);
-	 //print
-
-	 printf("%d! --\n",n);
-	 for (i=0;i<=9;i++){
-	    printf( "   (%d)%5d " , i , sum[i] ) ;
-	    if (i== 4) printf("\n");
-	 }
-	 printf("\n");
+        printf("%d! --\n", n);
+        for (i = 0; i <= 9; i ++)
+        {
+            printf("   (%d)%5d ", i, sum[i]);
+            if (i == 4) printf("\n");
+        }
+        printf("\n");
     }
 }
--- OLD_CODE_NO_VERIFY/Accepted/p333.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p333.cpp
@@ -1,63 +1,72 @@
-/* @JUDGE_ID:  12991WW  333  C++  */
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cctype>
 
-#include <stdio.h>
-#include <iostream.h>
-#include <string.h>
-#include <ctype.h>
+using namespace std;
 
-void pid(char id[1000]) {
-     int i;
-     for (i=0;i<=strlen(id)-1;i++){
-	 if ( id[i] !=' ') cout <<id[i];
-     }
+void pid(char id[1000])
+{
+    int i;
+    for (i = 0; i <= strlen(id) - 1; i ++)
+    {
+        if (id[i] != ' ') cout << id[i];
+    }
 }
 
+void iscorrect(char id[1000])
+{
+    int i;
+    int t = 0;
+    int sum[10] = {0};
+    char m;
 
-void iscorrect(char id[1000]){
-     int i;
-     int t=0;
-     int sum[10]={0};
-     char m;
+    for (i = 0; i <= strlen(id) - 1; i ++)
+    {
+        m = id[i];
+        if (isdigit(m) != 0)
+        {
+            t ++;
+            if (t > 10) break;
+            sum[t] = m - '0';
+        } else if (m == 'X')
+        {
+            t ++;
+            if (t > 10) break;
+            sum[t] = 10;
+        }
+    }
 
-     for (i=0;i<=strlen(id)-1;i++){
-	  m=id[i];
-	  if ( isdigit(m) !=0 ){
-	      t++;
-	      if (t>10) break;
-	      sum[t]=m-'0';
-	  }else if (m=='X' ){
-	      t++;
-	      if (t>10) break;
-	      sum[t]=10;
-	 }
-     }
+    if (t != 10)
+    {
+        pid(id);
+        cout << " is incorrect.\n";
+    } else
+    {
+        for (i = 2; i <= 10; i ++)
+        {
+            sum[i] += sum[i - 1];
+        }
+        for (i = 2; i <= 10; i ++)
+        {
+            sum[i] += sum[i - 1];
+        }
+        pid(id);
 
-     if (t !=10){
-	  pid(id);
-	  cout <<" is incorrect.\n";
-     }else{
-	  for (i=2;i<=10;i++){
-	      sum[i] += sum[i-1];
-	  }
-	  for (i=2;i<=10;i++){
-	      sum[i] += sum[i-1];
-	  }
-	  pid(id);
+        if (sum[10] % 11 == 0)
+            cout << " is correct.\n";
+        else
+            cout << " is incorrect.\n";
 
-	  if ( sum[10] % 11 == 0 )
-	       cout <<" is correct.\n";
-	  else
-	       cout <<" is incorrect.\n";
-
-     }
+    }
 }
 
+main()
+{
+    char id[1000];
 
-main(){
-     char id[1000];
-
-
-     while (gets(id) !=NULL){
-	 iscorrect(id);
-     }
+    while (gets(id) != NULL)
+    {
+        iscorrect(id);
+    }
 }
