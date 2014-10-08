159fddcee3934d702a1ffcea647317da3480d83c
wbkuo <et282523@hotmail.com>
2014-10-09 01:21:33 +0800

    暫存

--- OLD_CODE_NO_VERIFY/Accepted/p694.cpp
+++ OLD_CODE_NO_VERIFY/Accepted/p694.cpp
@@ -1,33 +1,39 @@
-/* @JUDGE_ID:  12991WW  694  C++  "Easy algorithm" */
+#include <iostream>
+#include <cstring>
+#include <cmath>
 
-#include <iostream.h>
-#include <string.h>
-#include <math.h>
+using namespace std;
 
-long val(double n,double limit){
-     long t;
-     for (t=1;n !=1;t++){
-	 if ( fmod (n,2)  ==0){
-	     n /=2.0;
-	 }else{
-	     n=n * 3.0 + 1.0;
-	 }
-	 if (n>limit) break;
-     }
-     return t;
+long val(double n, double limit)
+{
+    long t;
+    for (t = 1; n != 1; t++)
+    {
+        if (fmod(n, 2) == 0)
+        {
+            n /= 2.0;
+        }
+        else
+        {
+            n = n * 3.0 + 1.0;
+        }
+        if (n > limit) break;
+    }
+    return t;
 }
 
-main(){
-    int t=0;
+main()
+{
+    int t = 0;
     unsigned long v;
-    double a,limit;
+    double a, limit;
 
-    while (cin >>a>>limit !=NULL){
-	t++;
-	if (a<0) break;
-	v=val(a,limit);
-	cout <<"Case 1: A = "<<a<<", limit = "<<limit;
-	cout <<", number of terms = "<<v<<"\n";
-
+    while (cin >> a >> limit != NULL)
+    {
+        t++;
+        if (a < 0) break;
+        v = val(a, limit);
+        cout << "Case 1: A = " << a << ", limit = " << limit;
+        cout << ", number of terms = " << v << "\n";
     }
 }
