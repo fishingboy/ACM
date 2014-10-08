f4269d449b3fa48cf39b1f33de8123c1a2cd5392
wbkuo <et282523@hotmail.com>
2014-10-09 00:57:48 +0800

    [Verify] 583 - Prime Factors

---
+++ Accept/583.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void pp(long n)
+{
+    long i;
+    long be = 2;
+    long nn;
+    nn = sqrt(n);
+    for (i = be; i <= nn; i++)
+    {
+        if (n % i == 0)
+        {
+            n = n / i;
+            cout << i;
+            be = i;
+            break;
+        }
+    }
+    if (i > nn)
+    {
+        cout << n;
+        return;
+    }
+
+    while (n != 1)
+    {
+        for (i = be; i <= nn; i++)
+        {
+            if (n % i == 0)
+            {
+                n = n / i;
+                cout << " x " << i;
+                be = i;
+                break;
+            }
+        }
+        if (i > nn)
+        {
+            cout << " x " << n;
+            break;
+        }
+    }
+}
+
+void pp2(long n)
+{
+    long i;
+    long be = 2;
+    long nn;
+    cout << "-1";
+    n = n*-1;
+    nn = sqrt(n);
+    while (n != 1)
+    {
+        for (i = be; i <= nn; i++)
+        {
+            if (n % i == 0)
+            {
+                n = n / i;
+                cout << " x " << i;
+                be = i;
+                break;
+            }
+        }
+        if (i > nn)
+        {
+            cout << " x " << n;
+            break;
+        }
+    }
+}
+
+main()
+{
+    long n;
+    long i;
+
+    while (cin >> n, n != 0)
+    {
+        cout << n << " = ";
+        if (n > 1)
+            pp(n);
+        else if (n < 0)
+            pp2(n);
+        else if (n == 1)
+            cout << "1";
+        cout << "\n";
+    }
+}
