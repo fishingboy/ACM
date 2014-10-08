007dfce1f954a6bbce8a3d532d60f68534570cb5
wbkuo <et282523@hotmail.com>
2014-10-09 00:55:04 +0800

    [Verify] 573 -  The Snail

---
+++ Accept/573.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    double h, u, d, f, uu;
+    double sum;
+    long t;
+
+    while (cin >> h >> u >> d >> f, h != 0)
+    {
+        sum = t = 0;
+        uu = u;
+        while (1)
+        {
+            t++;
+            sum = sum + u;
+            if (sum > h) break;
+            sum = sum - d;
+            if (sum < 0) break;
+            u = u - uu * f / 100;
+            if (u < 0) u = 0;
+        }
+        
+        if (sum < 0)
+            cout << "failure on day " << t << "\n";
+        else
+            cout << "success on day " << t << "\n";
+    }
+}
