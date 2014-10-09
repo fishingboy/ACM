f1a40689538d7f7dada872f3e064c543f34d3867
wbkuo-nb <et282523@hotmail.com>
2014-10-09 15:49:47 +0800

    [Verify] 11875 - Brick Game

--- OLD_CODE_NO_VERIFY/Accepted/acm/accepted/11875.c
+++ OLD_CODE_NO_VERIFY/Accepted/acm/accepted/11875.c
@@ -6,31 +6,31 @@ int main()
     int n, people_count, age[11], x, i, j, k, avg;
 
     fscanf(stdin, "%d", &n);
-    for (i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
         fscanf(stdin, "%d", &people_count);
-        for (j=0; j<people_count; j++)
+        for (j = 0; j < people_count; j++)
         {
-		    fscanf(stdin, "%d", &age[j]);
+            fscanf(stdin, "%d", &age[j]);
         }
-	
-		avg = people_count/2 +1;
-        for (j=0; j<people_count; j++)
+
+        avg = people_count / 2 + 1;
+        for (j = 0; j < people_count; j++)
         {
-	    	x = 0;
-		    for (k=0; k<people_count; k++)
-    	    {
-				if (abs(x) > avg) break;
-		        if (age[j] > age[k]) 
-				    x++;
-        	    else if (age[j] < age[k])
-				    x--;
+            x = 0;
+            for (k = 0; k < people_count; k++)
+            {
+                if (abs(x) > avg) break;
+                if (age[j] > age[k])
+                    x++;
+                else if (age[j] < age[k])
+                    x--;
             }
             if (x == 0)
             {
-				printf("Case %d: %d", i+1, age[j]);
+                printf("Case %d: %d", i + 1, age[j]);
             }
-		}
+        }
         printf("\n");
     }
     return 0;
