820b287823bffc71a21d5b2ac951d366e32e94c9
wbkuo-nb <et282523@hotmail.com>
2014-10-09 15:15:41 +0800

    [Verify] 11364 - Optimal Parking

---
+++ Accept/11364.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int case_n, i, j, n, pos, max, min;
+
+    fscanf(stdin, "%d", &case_n);
+    for (i = 0; i < case_n; i++)
+    {
+        fscanf(stdin, "%d", &n);
+        min = 999;
+        max = 0;
+        for (j = 0; j < n; j++)
+        {
+            fscanf(stdin, "%d", &pos);
+            if (pos > max) max = pos;
+            if (pos < min) min = pos;
+        }
+        printf("%d\n", (max - min) * 2);
+    }
+
+    return 0;
+}
