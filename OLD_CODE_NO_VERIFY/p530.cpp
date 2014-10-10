1078750655feba5ed43328230a08ea6988a01375
wbkuo-nb <et282523@hotmail.com>
2014-10-11 00:57:18 +0800

    修正為可執行版本，不過速度似乎太慢了

--- OLD_CODE_NO_VERIFY/p530.cpp
+++ OLD_CODE_NO_VERIFY/p530.cpp
@@ -1,18 +1,28 @@
-#include <iostream.h>
+#include <iostream>
 
-long nCr(long n,long r){
-	if (n==r || r==0){
-		return 1;
-	}else if (r==1){
-		return n;
-	}else{
-		return nCr(n-1,r-1) + nCr(n-1,r);
-	}
+using namespace std;
+
+long long int nCr(long long int n, long long int r)
+{
+    if (n == r || r == 0)
+    {
+        return 1;
+    }
+    else if (r == 1)
+    {
+        return n;
+    }
+    else
+    {
+        return nCr(n - 1, r - 1) + nCr(n - 1, r);
+    }
 }
 
-void main(){
-	long  n,m;
-	while (cin >>n>>m){
-		cout << nCr(n,m)<<endl;
-	}
+main()
+{
+    long long int n, m;
+    while (cin >> n >> m)
+    {
+        cout << nCr(n, m) << endl;
+    }
 }
